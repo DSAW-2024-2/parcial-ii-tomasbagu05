ee13787654f34d2eb2b6716e1d368c671cff8fe7
Tomas <tomasbarriosguevara@gmail.com>
2024-10-04 08:43:44 -0500

    serverCommit

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ app.use(express.json());
 app.use(cors());
 
 //Usar los routers
-app.use('./api/auth', authRoutes);
-app.use('./api/weather', weatherRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/weather', weatherRoutes);
 
 app.use((req, res) => {
     res.status(404).json({ error: 'Not found' });
